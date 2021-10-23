1c9f989a5fa494d225e4a19ada422e275eb8f530
swat21072 <swat21072@gmail.com>
2021-10-23 11:37:09 +0700

    13131

--- todo.js
+++ todo.js
@@ -35,43 +35,41 @@ var save = document.getElementsByClassName("save")[0];  // кнопка сохр
 save.onclick = function() {
 
   writenote();
+  
 
   modal.style.display = "none";
+  document.getElementById("heading").value = "";
+  document.getElementById("note").value = "";
   
 }
 
 
 
-function delet(N) {
-  
-  var note =  document.getElementById('N');
-  
-  
-  note.parentNode.removeChild(note);
-
+function delet(val) {
+  var kk = parseInt(val.match(/\d+/));
+  var note = document.getElementById(kk);
+  note.remove();
 }
 
 let N = 0;
-
 function writenote(){   
 
   const keep = [];
   let i = 0;
   const keep2 =[];
-  
+  let but = 'but'+N;
 
   keep2.push(document.getElementById("heading").value);  // запись заголовка
   var list = document.getElementById('output')
   var note = document.createElement('note');
   note.setAttribute("class","note");
-  note.setAttribute("id","N");
+  note.setAttribute("id",N);
   var h2 = document.createElement('H2')
   h2.setAttribute('id','h2')
   h2.innerHTML = keep2[i]
   list.appendChild(note)
   note.appendChild(h2);
    
-  
   keep.push(document.getElementById("note").value);  // запись заметки
   var li = document.createElement('LI')
   li.setAttribute("class","notee")
@@ -81,18 +79,19 @@ function writenote(){
 
   var button = document.createElement('button');
   button.setAttribute("class","delet");
-  button.setAttribute('onclick','delet(N)');
+  button.setAttribute('onclick','delet(this.id)');
   button.setAttribute("type","button");
+  button.setAttribute("id",but);
   button.innerHTML = "удалить";
   note.appendChild(button);
 
   var edit = document.createElement('button');
   edit.setAttribute("class","delet");
-  edit.setAttribute('onclick','edit(N)');
+  edit.setAttribute('onclick','edit(this.id)');
   edit.setAttribute("type","button");
+  edit.setAttribute("id",but);
   edit.innerHTML = 'редактировать';
   note.appendChild(edit);
-
 
   N++;
   i++;
@@ -100,29 +99,39 @@ function writenote(){
 }
 
 
-function edit(N){
+function edit(val){
 
-  var note = document.getElementById('N')
+  var kk = parseInt(val.match(/\d+/));
+  var note = document.getElementById(kk);
+  
   
   var h2 = note.childNodes[0].firstChild;
   var li = note.childNodes[1].firstChild;
- 
+
   var input = document.createElement('input');
-  input.setAttribute('type','text');
-  input.setAttribute('class','edit');
-  input.setAttribute('size','60');
-  input.setAttribute('raw','30');
+  input.setAttribute("class",'edith');
+  input.setAttribute('id','hedit')
+  input.value=h2.nodeValue;
+  note.appendChild(input);
+
+  // var br = document.createElement('br')
+  // br.innerHTML = "<br>";
+  // note.appendChild(br);
+
+  var text=document.createElement('textarea');
+  text.setAttribute('class','editli');
+  text.setAttribute('id','liedit');
+  text.value=li.nodeValue;
+  note.appendChild(text);
+
+
+  var save = document.createElement('button');
+  save.setAttribute("class","delet");
+  save.setAttribute('onclick','saveedit(input,text)');
+  save.setAttribute("type","button");
+  save.innerHTML = 'сохранить';
+  note.appendChild(save);
   
-  note.childNodes[1].appendChild(input);
+
   
-
-
-
-
-
-
-
-  // h2.textContent= 'kek';
-
-
 }
